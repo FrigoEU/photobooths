ea1d1785373881b0bfc35f638cc9c01d37108a65
FrigoEU <simon.van.casteren@gmail.com>
2015-12-15 08:59:04 +0100

    Finished Networking

--- src/App/Networking.purs
+++ src/App/Networking.purs
@@ -14,20 +14,16 @@ import Data.Traversable
 import Data.Array (filter, elemIndex)
 import Data.Foreign
 import Data.Foreign.Class
-import DOM.File.Types(Blob())
 
 import Node.Buffer (Buffer(), BUFFER())
-import Node.FS.Aff (readdir, stat, mkdir, writeFile, rename)
-import Node.FS.Stats (isDirectory)
+import Node.FS.Aff (readdir, stat, mkdir, writeFile, rename, exists, rmdir, readFile, unlink)
 import Node.FS (FS())
-import Node.Path (normalize, concat)
-
-import Unsafe.Coerce
+import Node.Path (normalize, concat, FilePath(), resolve, basename)
 
 import Network.HTTP.Affjax 
 import Network.HTTP.Method (Method(..))
 
-import Control.Monad.Eff.Console (log)
+import Control.Monad.Eff.Console (log, CONSOLE())
 import Control.Monad.Eff.Class
 import Control.Monad.Aff (runAff)
 import Control.Monad.Error.Class (throwError, MonadError)
@@ -35,11 +31,12 @@ import Control.Monad.Eff.Exception (error, Error())
 import Control.Monad.Aff (Aff())
 import Control.Alt
 import Control.Bind (join)
-import Control.Apply((*>))
 
 import App.DB
 import App.Endpoint
+import App.Model.Photobooth
 import App.Model.Event
+import App.Model.SavedFile
 
 networkingConnectionInfo :: ConnectionInfo
 networkingConnectionInfo = Sqlite3
@@ -50,59 +47,87 @@ main = do
   let cname = "mycomputername"
   let baseurl = "http://localhost:8080"
   runAff (log <<< show) (const $ log "Everything synced!") $ withConnection networkingConnectionInfo $ \conn -> do
-    --dropDB conn
-    --makeDB conn
+    ------ For testing purposes ----- 
+    dropDB conn
+    makeDB conn
+    
     liftEff $ log "Initial SQL Done"
     
+    ------ Sync Photobooth ----------
+    oldPb <- queryPhotobooth conn cname
     pbm <- execEndpoint_ baseurl getPhotobooth cname unit
     case pbm of
          Nothing -> throwError $ error $ "No photobooth instance found for: " <> cname
          Just pb -> execute_ (upsertPB pb) conn
+    if oldPb /= pbm then rmdirRecur defaultDir else return unit
     liftEff $ log "Synced Photobooth(s)"
     
+    ------ Sync Events --------------
     maybeLastUpdatedEvents <- queryOne getLastUpdateEvents [] conn
     lastUpdatedEvents <- defaultDate maybeLastUpdatedEvents (fromStringStrict "2010-01-01T00:00:00.000Z")
     pes <- execEndpoint_ baseurl getNewEvents (Tuple cname lastUpdatedEvents) unit
+    traverse (\(PartialEvent {id: Just id}) -> rmdirRecur $ mkEventDir id) pes -- Event updated: regenerate folder
     withTransaction (\tc -> traverse (\pe -> execute_ (upsertPartialEvent pe) conn) pes) conn
     liftEff $ log "Synced Events"
     
+    ------ Sync Files  --------------
     maybeLastUpdatedFiles <- queryOne getLastUpdateFiles [] conn
     lastUpdatedFiles <- defaultDate maybeLastUpdatedFiles (fromStringStrict "2010-01-01T00:00:00.000Z")
     files <- execEndpoint_ baseurl getNewFiles (Tuple cname lastUpdatedFiles) unit
+    let eventIdsWithNewFiles = (\(SavedFile {eventId: i}) -> i) <$> files
+    traverse (\i -> rmdirRecur $ mkEventDir i) eventIdsWithNewFiles -- File updated: regenerate event folder
     withTransaction (\tc -> traverse (\sf -> execute_ (upsertSavedFile sf) conn) files) conn
     liftEff $ log "Synced Files"
     
+    ------ Sync File contents  --------------
     eventids <- query getEmptyFileEntries [] conn
     flip traverse eventids (\(WrappedId id) -> downloadFileById baseurl id >>= (\b -> saveFile conn id b))
     liftEff $ log "Synced File contents"
     
+    ------ Sync statistics back to master  --------------
     stats <- queryAllStatistics conn cname
     execEndpoint_ baseurl postStatistics unit stats
     liftEff $ log "Synced statistics"
     
+    ------ Make default profile folder -------------
+    defaultFolderExists <- exists defaultDir
+    if (not defaultFolderExists) 
+       then case pbm of
+                 Nothing -> return unit
+                 Just (Photobooth {defaultprofile}) -> do 
+                   mkdir defaultDir
+                   downloadFolder baseurl cname defaultprofile defaultDir
+       else return unit
+         
+                                 
+    
+    ------ Make folders for events --------------
+    safeMkdir (normalize "clientprofiles")
     events :: Array PartialEvent <- query getEventsByCname [toSql cname] conn
-    eventFolders :: Array String <- (filter (\s -> take 6 s == "event_")) <$> readdir (normalize "./clientprofiles")
+    eventFolders :: Array String <- (filter (\s -> take 6 s == "event_")) <$> readdir (normalize "clientprofiles")
     let eventIdsFromFolders = (\i -> maybe (-1) id $ safeParseInt i) <<< (drop 6) <$> eventFolders
     let missingEvents = filter (\(PartialEvent {id: Just id}) -> isNothing $ elemIndex id eventIdsFromFolders) events
-    liftEff $ log $ "Making event folders for: " <> (joinWith ", " $ show <<< (\(PartialEvent {id: Just id}) -> id) <$> missingEvents)
+    liftEff $ log $ "Making event folders for: " <> (joinWith ", " $ (\(PartialEvent {name: name}) -> name) <$> missingEvents)
     
     flip traverse missingEvents (\(PartialEvent ev) -> do 
       id <- maybe (throwError $ error $ "No id for PartialEvent") return ev.id
-      let target = "event_" <> show id
-      let temptarget = target <> "tmp"
-      mkdir (concat ["clientprofiles", temptarget])    
-      downloadFolder cname ev.profile temptarget
+      let target = mkEventDir id
+      let temptarget = mkTempEventDir id -- Writing first to temp folder to have some kind of "transaction"
+      rmdirRecur temptarget
+      mkdir temptarget
+      downloadFolder baseurl cname ev.profile temptarget
       buffers <- getFilesForEvent conn id
-      flip traverse buffers (\(NamedBuffer n f) -> writeFile (concat [temptarget, n]) f) -- Test if this OVERWRITES!!
-      rename temptarget target -- Test if this renames folders!
+      flip traverse buffers (\(NamedBuffer n f) -> overWriteFile (concat [temptarget, n]) f)
+      filesToCopy <- readdir temptarget
+      copyDir temptarget target
+      rmdirRecur temptarget
       liftEff $ log $ "Made folder for event " <> show id
-      )
+    )
     
    
    
    
    
-    -- *TODO*: Delete event folder when downloading/updating events AND files 
     
     
 data LastUpdated = LastUpdated String
@@ -114,6 +139,43 @@ instance isForeignLastUpdated :: IsForeign LastUpdated where
     
 toStr :: LastUpdated -> String
 toStr (LastUpdated s) = s
+
+mkEventDir :: Int -> FilePath
+mkEventDir i =  concat ["clientprofiles", "event_" <> show i]
+
+mkTempEventDir :: Int -> FilePath
+mkTempEventDir i = concat ["clientprofiles", "tmp_event_" <> show i]
+
+defaultDir :: FilePath
+defaultDir = concat ["clientprofiles", "default"]
+
+-- mkdir throws if the directory already exists, safeMkdir doesn't
+safeMkdir :: forall eff. FilePath -> Aff (fs :: FS | eff) Unit
+safeMkdir f = exists f >>= (\doesExist -> if (not doesExist) then mkdir f else return unit)
+
+-- rmdir doesn't remove folders when they're not empty, rmdirRecur does
+rmdirRecur :: forall eff. FilePath -> Aff (fs :: FS | eff) Unit
+rmdirRecur d = do 
+  exist <- exists d
+  if (not exist) then return unit
+                 else do filesToDelete <- readdir d
+                         traverse (\f -> unlink $ concat [d, f]) filesToDelete
+                         rmdir d
+  
+overWriteFile :: forall eff. FilePath -> Buffer -> Aff (fs :: FS, buffer :: BUFFER | eff) Unit
+overWriteFile f b = do
+  exist <- exists f
+  if (exist) then unlink f
+             else return unit
+  writeFile f b 
+
+copyDir :: forall eff. FilePath -> FilePath -> Aff (fs :: FS, buffer :: BUFFER | eff) Unit
+copyDir from to = do
+  rmdirRecur to
+  filesToCopy <- readdir from
+  mkdir to
+  traverse (\f -> readFile (concat [from, f]) >>= writeFile (concat [to, f])) filesToCopy
+  return unit
 
 data WrappedId = WrappedId Int
 instance isForeignWrappedId :: IsForeign WrappedId where
@@ -136,12 +198,12 @@ getEmptyFileEntries = Query $ "select id from files where file is NULL"
 getEventsByCname :: Query PartialEvent
 getEventsByCname = Query $ "select * from events where computername = ?"
                                                        
-downloadFolder :: forall eff. String -> String -> String -> Aff (ajax :: AJAX, fs :: FS, buffer :: BUFFER | eff) Unit
-downloadFolder cname profile target = do
-  files <- execEndpoint getProfileFiles (Tuple cname profile) unit
-  let makereq fn = {headers: [], content: Nothing, method: GET, url: fn, username: Nothing, password: Nothing } :: AffjaxRequest Unit
+downloadFolder :: forall eff. String -> String -> String -> String -> Aff (ajax :: AJAX, fs :: FS, buffer :: BUFFER, console :: CONSOLE | eff) Unit
+downloadFolder baseurl cname profile target = do
+  files <- execEndpoint_ baseurl getProfileFiles (Tuple cname profile) unit
+  let makereq fn = {headers: [], content: Nothing, method: GET, url: baseurl ++ "/" ++ fn, username: Nothing, password: Nothing } :: AffjaxRequest Unit
   traverse (\filename -> do {response} :: AffjaxResponse Buffer <- affjax $ makereq filename
-                            writeFile (concat [target, filename]) response) files
+                            writeFile (concat [target, basename filename]) response) files
   return unit
                                                
 data NamedBuffer = NamedBuffer String Buffer
@@ -153,7 +215,7 @@ instance isForeignNamedBuffer :: IsForeign NamedBuffer where
     return $ NamedBuffer name $ unsafeFromForeign file
                                                
 getFilesForEvent :: forall eff. Connection -> Int -> Aff (db :: DB | eff) (Array NamedBuffer)
-getFilesForEvent c i = query (Query "select file from files where eventid = ?") [toSql i] c
+getFilesForEvent c i = query (Query "select file, name from files where eventid = ?") [toSql i] c
                                                           
 downloadFileById :: forall eff. String -> Int -> Aff (ajax :: AJAX | eff) Buffer
 downloadFileById s i = do
